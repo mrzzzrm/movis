4c401d77e03ee56b68a6a3986a0a0b09084ed756
mrzzzrm <mrzesn@t-online.de>
2015-12-23 11:14:18 +0100

    SDL2 and tiled

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 extern crate ovisbp;
+extern crate tiled;
+extern crate sdl2;
+extern crate sdl2_image;
+
+use std::path::Path;
+use std::fs::File;
+use self::sdl2_image::LoadTexture;
 
 pub struct MyLevel {
     pub name: String,
+    pub tileset_texture: sdl2::render::Texture,
 }
 
 impl ovisbp::Level for MyLevel {
@@ -39,7 +47,21 @@ impl ovisbp::Level for MyLevel {
 }
 
 impl MyLevel {
-    pub fn new(name: &str) -> MyLevel {
-        MyLevel{name: name.to_string()}
+    pub fn new(name: &str, renderer: &sdl2::render::Renderer) -> MyLevel {
+        let file = File::open(&Path::new("data/map.tmx")).unwrap();
+        let map = tiled::parse(file).unwrap();
+        let mut tileset_texture: Option<sdl2::render::Texture> = None;
+
+        for tileset in map.tilesets.iter() {
+            assert!(tileset.images.len() == 1);
+
+            println!("Tileset at {:?}", tileset.images[0].source);
+
+            tileset_texture = Some(renderer.load_texture(&Path::new(&("data/".to_string() + &tileset.images[0].source))).unwrap());
+        }
+
+        println!("{:?}", map);
+
+        MyLevel{name: name.to_string(), tileset_texture:tileset_texture.unwrap()}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,45 @@
 mod lib;
 
+extern crate sdl2;
+extern crate sdl2_image;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    sdl2_image::init(sdl2_image::INIT_PNG | sdl2_image::INIT_JPG);
+
+    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut renderer = window.renderer().build().unwrap();
+
+    renderer.set_draw_color(Color::RGB(255, 0, 0));
+    renderer.clear();
+
     let name = "HalloWeltbeffepdejaijsnd".to_string();
-    let mut level = lib::MyLevel::new(&*name);
+    let mut level = lib::MyLevel::new(&*name, &renderer);
     println!("{}", level.name);
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        renderer.copy(&level.tileset_texture, None, None);
+        renderer.present();
+    }
 }
